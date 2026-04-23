d533f9787d12dfb391a93d3e187c050d58cf34a6
Gazaax <quentintibo06@gmail.com>
2026-04-23 09:06:40 +0200

    Fix: hero animation

--- script/personnage.js
+++ script/personnage.js
@@ -1,15 +1,13 @@
 const heroChar = document.getElementById('hero-char');
+const heroSection = document.getElementById('hero');
 
-document.addEventListener('mousemove', e => {
-    const rect = document.getElementById('hero').getBoundingClientRect();
-    if (rect.top < window.innerHeight && rect.bottom > 0) {
+document.addEventListener('mousemove', (e) => {
 
-        const xRel = (e.clientX / window.innerWidth - 0.5) * 2;
-        const yRel = (e.clientY / window.innerHeight - 0.5) * 2;
+    const rect = heroSection.getBoundingClientRect();
+    if (rect.top > window.innerHeight || rect.bottom < 0) return;
 
-        heroChar.style.transform = `
-      translateX(calc(-50% +${xRel * 14}px))
-      translateY(${yRel * 8}px)
-    `;
-    }
-}) ;
+    const xRel = (e.clientX / window.innerWidth  - 0.5)*2;
+    const yRel = (e.clientY / window.innerHeight - 0.5)*2;
+
+    heroChar.style.transform = `translateX(${xRel * 18}px) translateY(${yRel * 10}px)`;
+});
